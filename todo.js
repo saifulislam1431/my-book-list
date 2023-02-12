229bd800e7f4b534e4b3b42b2315660a5a9b0846
Saiful Islam <sakibbhaiya143@gmail.com>
2023-02-12 21:11:35 +0600

    Function part added

---
+++ todo.js
@@ -0,0 +1,23 @@
+let count = 0;
+document.getElementById('btn').addEventListener('click', function() {
+    count += 1;
+    const bookName = document.getElementById('book-name');
+    const book = bookName.value;
+    const authorName = document.getElementById('author-name');
+    const author = authorName.value;
+
+    const tableBody = document.getElementById('table-body');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${count}</td>
+        <td>${book}</td>
+        <td>${author}</td>
+        <td>
+        <button id="btn-01">Delete</button>
+        <button id="btn-02">Awesome</button>
+        </td>
+    `
+    tableBody.appendChild(tr);
+    bookName.value = '';
+    authorName.value = '';
+})
